1dba59dcd61fd0eb8c7fd33fc63deb544aaffffe
Jecyu <jecyu.lin@gmail.com>
2020-03-15 18:47:07 +0800

    添加 vue-loader scoped 样式例子

---
+++ Vue/vue-loader/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="App">
+    {{ msg }}
+    <ParentA></ParentA>
+    <ParentB></ParentB>
+  </div>
+</template>
+
+<script>
+import ParentA from "./ParentA.vue";
+import ParentB from "./ParentB.vue"
+export default {
+  name: "App",
+  components: {
+    ParentA,
+    ParentB
+  },
+  data() {
+    return {
+      msg: "App"
+    }
+  }
+}
+</script>
+
+<style>
+.App {
+  color: #000;
+}
+</style>
---
+++ Vue/vue-loader/ParentB.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="ParentA">{{ msg }}</div>
+</template>
+
+<script>
+export default {
+  name: "ParentB",
+  data () {
+    return {
+      msg: 'ParentB'
+    }
+  }
+}
+</script>
+
+<style scoped>
+.ParentB {
+  color: blue;
+}
+</style>
